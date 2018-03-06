5d6adbae63026a3213ee8d8b31b578de76a2b2a9
theoneandonlysamduan <31659729+theoneandonlysamduan@users.noreply.github.com>
2018-03-06 16:19:53 -0500

    Add files via upload

---
+++ TimeModule.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from os import remove
+
+datetime = str(datetime.now())			#Fetching date & time in a long big string
+date = datetime[:10]					#Separates date and time
+time = datetime[11:]
+time =     time[:8 ]					#Gets rid of the milliseconds
+
+finalString = date + '\n' + time		#Produces a final string that looks like 2018-03-06\n16:12:21
+
+
+remove('Time')							#Removes the previous time file and makes a new one
+f = open('Time', 'w+')
+f.write(finalString)					#Writes the final string into the cache file
+
+
+print(date)
+print(time)
