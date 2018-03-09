9be060c13d6b63b73e652869b722ce5b193dfde2
theoneandonlysamduan <31659729+theoneandonlysamduan@users.noreply.github.com>
2018-03-09 18:49:58 -0500

    Add files via upload
    The Virtual Display Module is incorporated into the Central Module, triggered by terminal arguments "-vd". 
    Help will be pulled up with "-h". The help doc will later be created and incorporated into the Central Module instead of being hard coded into it.

--- CentralModule.py
+++ CentralModule.py
@@ -8,6 +8,7 @@ set_weather_interval = 200				#Interval between every update of weather
 #The DisplayModule is still work in progress. 
 
 print('Please visit https://www.wunderground.com/?apiref=c68c74f9a26733fa to provide me with free API access. Thanks!')
+print('Ctrl+C to quit the program.')
 
 def WeatherModule():
 	call(['python3 WeatherModule.py'], shell = True)
@@ -15,17 +16,30 @@ def WeatherModule():
 def TimeModule():
 	call(['python3 TimeModule.py'], shell = True)
 
-#This piece of code is used to detect the keystroke so that there is a way of quitting the program. Not working yet because it shuts the program down unless a key is pressed. 
-#def getch():
-#    fd = sys.stdin.fileno()
-#    old_settings = termios.tcgetattr(fd)
-#    try:
-#        tty.setraw(sys.stdin.fileno())
-#        ch = sys.stdin.read(1)
-# 
-#    finally:
-#        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-#    return ch
+def VirtualDisplay():
+	call(['python3 VirtualDisplay.py'], shell = True)
+
+
+#This chunk is to decide which display module to pull up. 
+args = sys.argv
+sig = True
+#If number of arguments is wrong, skip the while loop and end everything. 
+if len(args) > 2: 
+	sig = False
+	print('Wrong arguments. Please use -h to get help.')
+elif args[1] == '-vd': 		#If argument is -vd pull up Virtual Display Module
+	VirtualDisplay()
+elif args[1] == '-h':		#If argument is -h then pull up help. May wanna get it into a separate file. 
+	print('Help\n')
+	print('Argument \t Function')
+	print('-h \t\t Gets you to this page')
+	print('-vd \t\t Opens a window made with pygame with all of the information')
+	sig = False
+else: 
+	print('Wrong arguments. Please use -h to get help.')
+
+
+
 
 #Those two variables store the interval between the last and the current trigger of the time and weather modules. 
 interval_time 	 = 0
@@ -38,7 +52,7 @@ weatherCache = time.clock_gettime(time.CLOCK_REALTIME)//1
 TimeModule()
 WeatherModule()
 #print('Press "q" to quit')
-while True:
+while sig:
 	current_time = time.clock_gettime(time.CLOCK_REALTIME)//1
 	#interval_time records the current time from the last trigger of TimeModule. 
 	#interval_weather records the current time from the last trigger of WeatherModule. 
@@ -61,8 +75,3 @@ while True:
 		weatherCache = current_time
 	
 	time.sleep(0.5)				#halt for half a second to save sys resources
-	#if (key == 'q'):
-	#	print('Terminated.')
-	#	break
-	#if (key == ''): 
-	#	pass
